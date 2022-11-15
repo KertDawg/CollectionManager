474d9279c46c7ce6a1ce8c1f4c75057c3c74b3f5
Kertis Henderson <kertishenderson@gmail.com>
2022-11-14 22:32:29 -0500

    Added an app version insert.

--- server/controllers/Mainenance.php
+++ server/controllers/Mainenance.php
@@ -10,14 +10,42 @@ class Maintenance
 	public function FirstTimeSetup()
 	{
 		$Password = "abc123";
+		$DoWeNeedToSetup = $FALSE;
 
-		//  Add an admin user.
+		//  Do we need to do this?
 		$bq = \utils\Database::GetDB()->prepare("
-		INSERT INTO User 
-		(UserID, UserName, Admin, Active, PasswordHash) 
-		VALUES 
-		(?, 'admin', 1, 1, ?);
+		SELECT COUNT(*) AS UserCount
+		FROM User;
 		");
 		$bq->execute([\utils\Database::GUID(), password_hash($Password, PASSWORD_DEFAULT)]);
+
+		foreach($bq as $b)
+		{
+			if ($b["ItemID"] < 1)
+			{
+				$DoWeNeedToSetup = $TRUE;
+			}
+		}
+
+		if ($DoWeNeedToSetup)
+		{
+			//  Add an admin user.
+			$bq = \utils\Database::GetDB()->prepare("
+			INSERT INTO User 
+			(UserID, UserName, Admin, Active, PasswordHash) 
+			VALUES 
+			(?, 'admin', 1, 1, ?);
+			");
+			$bq->execute([\utils\Database::GUID(), password_hash($Password, PASSWORD_DEFAULT)]);
+
+			//  Add an app version.
+			$bq = \utils\Database::GetDB()->prepare("
+			INSERT INTO Setting 
+			(Key, Value) 
+			VALUES 
+			('AppVersion', '');
+			");
+			$bq->execute([]);
+		}
 	}
 }
