e20df0142a54c9708ca9cc5382c9f6b29adf2847
Kertis Henderson <kertishenderson@gmail.com>
2022-11-22 21:12:13 -0500

    Docker API configuration file

---
+++ server/Configuration-Docker.php
@@ -0,0 +1,9 @@
+<?php
+
+global $Configuration;
+
+$Configuration["DBHost"] = "127.0.0.1";
+$Configuration["DBSchema"] = "Collections";
+$Configuration["DBUser"] = "collections";
+$Configuration["DBPassword"] = "Th3D@t3rbaSePa#s";
+$Configuration["TokenKey"] = "TheDayIsL0ng";
